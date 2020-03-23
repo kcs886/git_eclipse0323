0ce49094eff968ae6edf49f6cc8113db6092d074
kcs886 <rlacndtlr7482@gmail.com>
2020-03-23 16:58:50 +0900

    123123123

---
+++ dev_book/src/hotfix/HotfixTest4.java
@@ -0,0 +1,10 @@
+package hotfix;
+
+public class HotfixTest4 {
+
+	public static void main(String[] args) {
+	//hotfix브렌치에서 저장점을 4개더 가지기 위한 소스 추가
+		System.out.println("hotfix 브렌치에서 4번째로 추가한 소스임.");
+	}
+
+}
