d3a5b861b33958e84c6b831c004e364ba484f6bd
kcs886 <rlacndtlr7482@gmail.com>
2020-03-23 16:42:32 +0900

    hotfix브렌치에서 저장점을 세개더 가지기 위한 소스 추가

---
+++ dev_book/src/hotfix/HotfixTest3.java
@@ -0,0 +1,10 @@
+package hotfix;
+
+public class HotfixTest3 {
+
+	public static void main(String[] args) {
+	//hotfix브렌치에서 저장점을 한개더 가지기 위한 소스 추가
+		System.out.println("hotfix 브렌치에서 추가한 소스임.");
+	}
+
+}
